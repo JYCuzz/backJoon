4180f123666f36f56f5ae632922f460c96f54a25
JiYeop Jang <107393446+JYCuzz@users.noreply.github.com>
2025-01-13 15:14:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.next();
+        
+        char b[] = a.toCharArray();
+        	
+        System.out.println((int)b[0]);
+    }
+}
