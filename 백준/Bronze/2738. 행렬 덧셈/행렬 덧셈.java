92be13e99afc4c10bdbbd496a3620addb6e92197
JiYeop Jang <107393446+JYCuzz@users.noreply.github.com>
2025-01-15 02:16:12 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 496 ms, Memory: 37192 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int m = sc.nextInt();
+    	
+    	int a[][] = new int[n][m];
+    	int b[][] = new int[n][m];
+    	
+    	for(int i = 0; i < a.length; i++) {
+    		
+    		for(int j = 0; j < a[i].length; j++) {
+    			
+    			a[i][j] = sc.nextInt();
+    		}
+    	}
+    	
+    	for(int i = 0; i < b.length; i++) {
+    		
+    		for(int j = 0; j < b[i].length; j++) {
+    			
+    			b[i][j] = sc.nextInt();
+    		}
+    	}
+    	
+    	for(int i = 0; i < b.length; i++) {
+    		
+    		for(int j = 0; j < b[i].length; j++) {
+    			
+    			System.out.print(a[i][j] + b[i][j] + " ");
+    		}
+    		System.out.println("");
+    	}
+    }
+}
