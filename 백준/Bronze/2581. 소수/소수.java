b59d0c0cca54140c8cfe8195bcb9d516b8553451
JiYeop Jang <107393446+JYCuzz@users.noreply.github.com>
2025-02-13 20:41:37 +0900

    [Bronze II] Title: 소수, Time: 336 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int M = sc.nextInt();
+    	int N = sc.nextInt();
+    	int sum = 0;
+    	int min = N;
+    	
+    	for(int i = M; i <= N; i++) {
+    		
+    		int count = 0;
+    		
+    		for(int j = 1; j <= i; j++) {
+    			
+    			if(i % j == 0) {
+    				count++;
+    			}
+    		}
+    			
+    		if(count == 2) {
+    			
+    			if(i < min) {
+    				min = i;
+    			}
+    			sum += i;
+    		}
+    		
+    	}
+    	
+    	if(sum == 0) {
+    		System.out.println(-1);
+    	}
+    	else {
+	    	System.out.println(sum);
+	    	System.out.println(min);
+    	}
+    }
+}
