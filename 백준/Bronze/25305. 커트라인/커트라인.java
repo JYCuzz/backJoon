1871ee27fa725b017ce884bd5bc9e49d35a8e3cb
JiYeop Jang <107393446+JYCuzz@users.noreply.github.com>
2025-02-16 17:08:23 +0900

    [Bronze II] Title: 커트라인, Time: 224 ms, Memory: 19400 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    	
+    	Scanner sc = new Scanner(System.in);
+
+    	int n = sc.nextInt();
+    	int k = sc.nextInt();
+    	
+    	int array[] = new int[n];
+    	
+    	for(int i = 0; i < n; i++) {
+    		array[i] = sc.nextInt();
+    	}
+    	
+    	Arrays.sort(array);
+    	
+    	int count = 0;
+    	
+    	for(int i = n-1; i >= 0; i--) {
+    		
+    		count++;
+    		
+    		if(count == k) {
+    			System.out.println(array[i]);
+    		}
+    	}
+    }
+}
